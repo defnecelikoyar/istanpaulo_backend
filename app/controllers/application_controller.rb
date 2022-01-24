4b5b827963fd7a7a15f7df23808172da238b8b2c
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-01-24 17:14:22 -0500

    minor update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/artworks" do
+    { message: "The gallery!" }.to_json
+  end
+
 end
